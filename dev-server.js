b48e22d837138338e7cfdd7fc830972904553330
brnstinton-lab <brnstinton@gmail.com>
2026-09-12 14:26:21 +0000

    Remove old React frontend

--- dev-server.js
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'child_process';
-
-const py = spawn('python3', ['backend/manage.py', 'runserver', '0.0.0.0:3000'], {
-  stdio: 'inherit'
-});
-
-py.on('exit', (code) => {
-  process.exit(code || 0);
-});
